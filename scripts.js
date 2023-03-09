6ce4b36aab5fb9b5db7b7b8e7cff4b8ea9868bb1
teglus133 <111870922+teglus133@users.noreply.github.com>
2023-03-09 10:30:33 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+var idMate = document.getElementById("mate");
+var idLengua = document.getElementById("lengua");
+var idEfsi = document.getElementById("efsi");
+
+
+
+
+function ValidarNota(event) {
+    var numero = document.getElementById("nota").value;
+    console.log(numero);
+    if (numero > 0 && numero < 11) {
+        document.getElementById("nota").style.color = "green";
+    } else {
+        document.getElementById("nota").style.color = "red";
+    }
+}
+
